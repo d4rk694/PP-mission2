2ad704dd6de26b0e9f4067ca3026464cfac5d953
Ricci Dorian <dorian.ricci@outlook.com>
2018-03-18 19:10:59 +0100

    define init and toString methode

--- code/feature_execution/feature_selector.rb
+++ code/feature_execution/feature_selector.rb
@@ -3,5 +3,13 @@ class FeatureSelector
 	attr_reader :feature, :klass
 
 	# TODO To be completed
+	def initialize(feature, klass)
+		@feature = feature
+		@klass = klass
+	end
+
+	def to_s()
+		@feature + " " + @klass
+	end
 
 end
