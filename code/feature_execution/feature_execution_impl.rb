e934feea09ea7407be444cf7732fc3a37bbf59c0
Ricci Dorian <dorian.ricci@outlook.com>
2018-03-19 15:49:36 +0100

    test proceed

--- code/feature_execution/feature_execution_impl.rb
+++ code/feature_execution/feature_execution_impl.rb
@@ -1,56 +1,60 @@
 require 'singleton'
 
- # classes = []
+# classes = []
 
 def adapt_classes(klass)
 
 end
 
-Dir["#{File.dirname(__FILE__)}/../text_correctness_app/features/*.rb"].each { |file| require file }
-Dir["#{File.dirname(__FILE__)}/../text_correctness_app/skeleton/*.rb"].each { |file| require file }
+Dir["#{File.dirname(__FILE__)}/../text_correctness_app/features/*.rb"].each {|file| require file}
+Dir["#{File.dirname(__FILE__)}/../text_correctness_app/skeleton/*.rb"].each {|file| require file}
 
 
 def proceed
-  # puts "self == #{self}"
-  self.text
+  puts "memory == #{FeatureExecutionImpl.instance.memory}"
+  puts
+  FeatureExecutionImpl.instance.next_op(self, caller_locations(1,1)[0].label)
 end
+
+
 
 class FeatureExecutionImpl
 
-  attr_reader :memory, :last_feature
+  include Singleton
+
+  attr_reader :memory, :last_feature, :stack_op
 
   def initialize
     @memory = Hash.new
-    @last_feature = nil
+    @last_feature = Hash.new
+    @stack_op = Array.new
   end
 
-
-	include Singleton
-
-	def alter(action, feature_selector)
-		if action == :adapt
-			adapt(feature_selector)
+  def alter(action, feature_selector)
+    if action == :adapt
+      adapt(feature_selector)
     elsif action == :unadapt
       unadapt(feature_selector)
-		end
-		# TODO To be completed
-	end
+    end
+    # TODO To be completed
+  end
 
   # TODO To be completed if needed
-	def adapt(feature_selector)
-		# puts "cpt = #{@cpt}"
+  def adapt(feature_selector)
+    # puts "cpt = #{@cpt}"
     module_obj = Object.const_get(feature_selector.feature)
-		class_obj = Object.const_get(feature_selector.klass)
-		methods_names_array = module_obj.instance_methods
+    class_obj = Object.const_get(feature_selector.klass)
+    methods_names_array = module_obj.instance_methods
 
     # check if the klass has been saved
-    unless @memory.key(feature_selector.klass)
+    unless @memory[feature_selector.klass]
+      puts "\tCreate new hash for #{feature_selector.klass}"
       # puts "create #{feature_selector.klass}"
       @memory[feature_selector.klass] = Hash.new
     end
 
     # foreach methods in the module
-		methods_names_array.each do |method_item|
+    methods_names_array.each do |method_item|
 
       begin
         old_method = class_obj.instance_method(method_item)
@@ -61,17 +65,20 @@ class FeatureExecutionImpl
       # check if the method already exist ?
       unless old_method.nil?
         # check if the method has been already saved before ?
-        unless @memory[feature_selector.klass][method_item]
+        unless @memory[feature_selector.klass]["#{method_item}"]
           # puts "create #{method_item}"
-          @memory[feature_selector.klass][method_item] = []
+          @memory[feature_selector.klass]["#{method_item}"] = []
         end
         # save the old method
-        @memory[feature_selector.klass][method_item].push({"#{@last_feature}" => old_method})
+        @memory[feature_selector.klass]["#{method_item}"].push({"#{@last_feature[feature_selector.klass]}" => old_method})
       end
-			method_obj = module_obj.instance_method(method_item)
-			class_obj.send(:define_method, method_item, method_obj)
+      method_obj = module_obj.instance_method(method_item)
+      class_obj.send(:define_method, method_item, method_obj)
     end
-    @last_feature = feature_selector.feature
+
+    @last_feature[feature_selector.klass] = feature_selector.feature
+
+    puts "Alter => #{@memory}"
   end
 
   def unadapt(feature_selector)
@@ -85,24 +92,57 @@ class FeatureExecutionImpl
 
       method_obj = module_obj.instance_method(method_item)
 
-      if @last_feature == feature_selector.feature
+      if @last_feature[feature_selector.klass] == feature_selector.feature
 
         class_obj.send(:remove_method, method_item)
 
-        if @memory[feature_selector.klass] && @memory[feature_selector.klass][method_item]
-          method_old = @memory[feature_selector.klass][method_item].pop()
+        if @memory[feature_selector.klass] && @memory[feature_selector.klass]["#{method_item}"]
+          method_old = @memory[feature_selector.klass]["#{method_item}"].pop()
           if method_old
             method_old.each do |key, value|
               class_obj.send(:define_method, method_item, value)
-              @last_feature = key
+              @last_feature[feature_selector.klass] = key
             end
           end
         end
       end
 
 
-
-
     end
+  end
+
+  def next_op(sself, called_from)
+
+    klass = sself.class.name
+    meth = called_from
+    # puts "#{klass} - #{meth}"
+
+    unless @stack_op == nil
+      @stack_op = @memory.clone
+    end
+
+    puts "stack => #{@stack_op}"
+    puts "stack methods => #{@stack_op[klass][meth]}"
+
+    meth_to_call = @stack_op[klass][meth].pop()
+
+    if @stack_op[klass][meth].empty?
+      @stack_op[klass][meth] = @memory[klass][meth].clone
+    end
+
+
+    puts "method => #{meth_to_call}"
+
+    to_return = sself.text
+    # #TODO change
+    #
+    # klass_Obj = Object.const_get(klass)
+    #
+    # meth_to_call.each do |key, value|
+    #   klass_un = klass_Obj.instance_method(meth)
+    #
+    #   to_return = klass_un.bind(value).call()
+    # end
+    to_return
   end
 end
