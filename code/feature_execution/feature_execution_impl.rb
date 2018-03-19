15ec1a45fb20f39c9e96c66b0a6aeb1d7dded7bd
Ricci Dorian <dorian.ricci@outlook.com>
2018-03-19 11:28:29 +0100

    add feature

--- code/feature_execution/feature_execution_impl.rb
+++ code/feature_execution/feature_execution_impl.rb
@@ -1,7 +1,11 @@
 require 'singleton'
+
+ # classes = []
+
 def adapt_classes(klass)
-	puts klass
+
 end
+
 Dir["#{File.dirname(__FILE__)}/../text_correctness_app/features/*.rb"].each { |file| require file }
 Dir["#{File.dirname(__FILE__)}/../text_correctness_app/skeleton/*.rb"].each { |file| require file }
 
@@ -18,9 +22,17 @@ class FeatureExecutionImpl
 
 	# TODO To be completed if needed
 	def adapt(feature_selector)
-
-		puts feature_selector.klass.public_methods
-
-		puts "adapt " + feature_selector.to_s()
+		puts feature_selector.to_s()
+		moduleObj = Object.const_get(feature_selector.feature)
+		puts moduleObj
+		classObj = Object.const_get(feature_selector.klass)
+		puts classObj
+		methodesNamesArray = moduleObj.instance_methods
+		puts
+		methodesNamesArray.each do |item|
+			puts item
+			methodeObj = moduleObj.instance_method(item)
+			classObj.send(:define_method, item, methodeObj )
+		end
 	end
 end
