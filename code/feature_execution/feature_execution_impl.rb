bf4e605c25e1d38b749210082ecdb1d2f90ecd6e
Ricci Dorian <dorian.ricci@outlook.com>
2018-03-20 10:16:40 +0100

    added comment to the code

--- code/feature_execution/feature_execution_impl.rb
+++ code/feature_execution/feature_execution_impl.rb
@@ -41,8 +41,10 @@ class FeatureExecutionImpl
   end
 
   # TODO To be completed if needed
+  #
   def adapt(feature_selector)
     # puts "cpt = #{@cpt}"
+    #variables
     module_obj = Object.const_get(feature_selector.feature)
     class_obj = Object.const_get(feature_selector.klass)
     methods_names_array = module_obj.instance_methods
@@ -57,6 +59,7 @@ class FeatureExecutionImpl
     # foreach methods in the module
     methods_names_array.each do |method_item|
 
+      # test if the method exists in the class
       begin
         old_method = class_obj.instance_method(method_item)
       rescue
@@ -73,19 +76,26 @@ class FeatureExecutionImpl
         # save the old method
         @memory[feature_selector.klass]["#{method_item}"].push({"#{@last_feature[feature_selector.klass]}" => old_method})
       end
+
+      # add the method to the class
       method_obj = module_obj.instance_method(method_item)
       class_obj.send(:define_method, method_item, method_obj)
     end
 
+    # remember the last feature added for the klass
     @last_feature[feature_selector.klass] = feature_selector.feature
 
-    puts "Alter => #{@memory}"
+    # puts "Alter => #{@memory}"
   end
 
+
+
+
   def unadapt(feature_selector)
+
+    # variables
     module_obj = Object.const_get(feature_selector.feature)
     class_obj = Object.const_get(feature_selector.klass)
-
     methods_names_array = module_obj.instance_methods
 
     # foreach methods of the module
@@ -94,9 +104,10 @@ class FeatureExecutionImpl
       method_obj = module_obj.instance_method(method_item)
 
       if @last_feature[feature_selector.klass] == feature_selector.feature
-
+        # remove the methode from the class
         class_obj.send(:remove_method, method_item)
 
+        # if there is a method in the history, define it in the class
         if @memory[feature_selector.klass] && @memory[feature_selector.klass]["#{method_item}"]
           method_old = @memory[feature_selector.klass]["#{method_item}"].pop()
           if method_old
@@ -107,43 +118,48 @@ class FeatureExecutionImpl
           end
         end
       end
-
-
     end
   end
 
   def next_op(sself, called_from)
 
+    # variables
     klass = sself.class.name
     meth = called_from
     # puts "#{klass} - #{meth}"
 
+    # copy the memory at first call
     unless @stack_op == nil
       @stack_op = @memory.clone
     end
 
-    puts "stack => #{@stack_op}"
-    puts "stack methods => #{@stack_op[klass][meth]}"
+    # puts "stack => #{@stack_op}"
+    # puts "stack methods => #{@stack_op[klass][meth]}"
 
+    #get the method that have to be called, based on the class and method calling
     meth_to_call = @stack_op[klass][meth].pop()
 
+    # if after a pop the history of the method is empty, we update the memory
     if @stack_op[klass][meth].empty?
       @stack_op[klass][meth] = @memory[klass][meth].clone
     end
 
 
-    puts "method => #{meth_to_call}"
+    # puts "method => #{meth_to_call}"
+
 
     to_return = sself.text
-    # #TODO change
-    #
-    # klass_Obj = Object.const_get(klass)
-    #
-    # meth_to_call.each do |key, value|
-    #   klass_un = klass_Obj.instance_method(meth)
-    #
-    #   to_return = klass_un.bind(value).call()
-    # end
+=begin
+    #TODO change
+
+    klass_Obj = Object.const_get(klass)
+
+    meth_to_call.each do |key, value|
+      klass_un = klass_Obj.instance_method(meth)
+
+      to_return = klass_un.bind(value).call()
+    end
+=end
     to_return
   end
 end
